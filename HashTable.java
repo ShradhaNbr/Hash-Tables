0d405d3e794c7fd9f493266b1b12747e3d52cef4
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-21 16:39:04 +0530

    Added : Code to remove a word

--- HashTable.java
+++ HashTable.java
@@ -3,7 +3,7 @@ package HashTable;
 public class HashTable {
 	public static void main(String[] args) {
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
-		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidabe situations";
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
 			Integer value = myHashMap.get(word);
@@ -13,6 +13,7 @@ public class HashTable {
 				value = value + 1;
 			myHashMap.add(word, value);
 		}
+		myHashMap.remove("avoidable");
 		System.out.println(myHashMap);
 	}
 }
